d99043fa5e6e4d7f09294b433a0b9dc25cf17a47
Daynlight <danielstodulski000@gmail.com>
2026-03-04 10:44:13 +0100

    AA

--- server/initial/src/main/java/com/example/springboot/Application.java
+++ server/initial/src/main/java/com/example/springboot/Application.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @SpringBootApplication
 public class Application {
 
+  @Value("${server.port}")
+  private int port;
+
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-    System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-    String[] beanNames = ctx.getBeanDefinitionNames();
-    Arrays.sort(beanNames);
-    for (String beanName : beanNames) {
-      System.out.println(beanName);
-    }
+    
+    Application app = ctx.getBean(Application.class);
+    System.out.println("Server is running on port: " + app.port);
   }
   
 }
